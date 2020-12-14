64faa2e561453d4dc884515edfc1b6e8f39d8da0
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-14 04:45:34 -0500

    commit tree 58d0e7c70c9fb3c3a3117a03b241de65f804006a

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -10,6 +10,7 @@ export default class ChromeBoisDomain extends Component {
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
+    drawChromeBoiAtCoords(event.clientX,event.clientY)
   }
   
   /* TODO: Create an event handler which, when fired, invokes the provided
@@ -23,10 +24,17 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
+
+  runResize = event => {
+    if (event.key === "a") resize("+")
+    else if (event.key === "s") resize("-")
+  }
   
   render() {
     return (
-      <canvas 
+      <canvas
+        onKeyPress={this.runResize}
+        onClick={toggleCycling}  
         onMouseMove={this.handleMouseMove}
         width='900'
         height='600'
